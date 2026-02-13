4161e200deed09e32a4076f45ab6e361a4af9550
Link4Electronics <link4electronics@gmail.com>
2026-02-13 13:12:50 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,7 +6,15 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-# pacman -Syu --noconfirm PACKAGESHERE
+pacman -Syu --noconfirm   \
+    freepats-general-midi \
+    libdecor              \
+    sdl2                  \
+    SDL2_mixer            \
+    SDL2_ttf              \
+    SDL2_image            \
+    SDL2_gfx              \
+    timidity++
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
@@ -16,11 +24,23 @@ get-debloated-pkgs --add-common --prefer-nano
 #make-aur-package PACKAGENAME
 
 # If the application needs to be manually built that has to be done down here
+echo "Making nightly build of OpenClaw..."
+echo "---------------------------------------------------------------"
+REPO="https://github.com/pjasicek/OpenClaw"
+VERSION="$(git ls-remote "$REPO" HEAD | cut -c 1-9 | head -1)"
+git clone --recursive --depth 1 "$REPO" ./OpenClaw
+echo "$VERSION" > ~/version
 
-# if you also have to make nightly releases check for DEVEL_RELEASE = 1
-#
-# if [ "${DEVEL_RELEASE-}" = 1 ]; then
-# 	nightly build steps
-# else
-# 	regular build steps
-# fi
+mkdir -p ./AppDir/bin
+cd ./OpenClaw
+mkdir -p build && cd build
+cmake .. \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_POLICY_VERSION_MINIMUM=3.5
+make -j$(nproc)
+cd ../Build_Release
+mv -v openclaw ../AppDir/bin
+mv -v SAVES.XML ../AppDir/bin
+mv -v clacon.ttf ../AppDir/bin
+mv -v config.xml ../AppDir/bin
+mv -v console02.tga ../AppDir/bin
